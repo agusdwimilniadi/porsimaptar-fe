605e391d738ba128e8cb0584d4bb6c8ace9ae727
agusdwimilniadi <agusdwimill@gmail.com>
2022-08-30 18:33:41 +0700

    push

---
+++ js/gsap.js
@@ -0,0 +1,32 @@
+gsap.registerPlugin(ScrollTrigger);
+
+$(document).ready(function () {
+  gsap.from("#judul-header", {
+    scale: 0,
+    transformOrigin: "center bottom",
+    duration: 1,
+    delay: 0.3,
+  });
+  gsap.to("#judul-header", {
+    scale: 1,
+    transformOrigin: "center bottom",
+    duration: 1,
+    delay: 1.2,
+  });
+  gsap.fromTo("#cloud-left", { x: -600 }, { x: 0, duration: 1.5, delay: 1.4 });
+  gsap.fromTo("#cloud-left-2", { x: 600 }, { x: 0, duration: 1.5, delay: 1.4 });
+  gsap.fromTo("#cloud-left-2", { x: 600 }, { x: 0, duration: 1.5, delay: 1.4 });
+  gsap.fromTo(".fire-cloud", { x: -600 }, { x: 0, duration: 1.5, delay: 1.4 });
+  gsap.fromTo(".fire-cloud-2", { x: 600 }, { x: 0, duration: 1.5, delay: 1.4 });
+
+  gsap.fromTo(
+    "#maskot-1",
+    { x: -600, y: 600 },
+    { x: 0, y: 0, duration: 1.5, delay: 2.4 }
+  );
+  gsap.fromTo(
+    "#maskot-2",
+    { x: 600, y: 600 },
+    { x: 0, y: 0, duration: 1.5, delay: 2.4 }
+  );
+});
